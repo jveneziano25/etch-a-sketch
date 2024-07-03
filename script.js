01d5be21397597fc18093163ae4108246ee7c8f4
Josh Veneziano <jveneziano@proton.me>
2024-07-03 15:42:45 -0500

    Rewrite logic to reliably get the correct size and layout of the grid area and the squares within.

--- script.js
+++ script.js
@@ -1,21 +1,69 @@
-//Creates initial 16x16 grid of squares 
-function createSquares(number) {
-    number = 255;
-    let i = 0;
-    
-    let initialGrid = document.getElementById("container");
-    while (i <= number) {
-        let newDiv = document.createElement("div");
-        newDiv.classList.add("squares");
-        newDiv.textContent = '';
-        let initialGrid = document.getElementById("container");
-        initialGrid.appendChild(newDiv);
-        i++;
-        newDiv.addEventListener("mouseenter", () => {
-            newDiv.style.backgroundColor = 'black';
-        });
+//Declares constant for grid size in order to distribute the squares accordingly
+const gridSide = 800;
+//These will be necessary to divide the gridSide variable to distribute the squares
+let rows = 16;
+let cols = 16;
+
+//sets the height and width of the gridArea
+const gridArea = document.querySelector("#container");
+gridArea.style.width = `${gridSide}px`;
+gridArea.style.height = `${gridSide}px`;
+
+/*Creates the square divs by iterating through the loop based on the number 
+of rows times the columns and sets the square div height and width accordingly.*/
+function sixteenBySixteen() {
+    for (let i = 0; i < (rows * cols); i++) {
+        let cells = document.createElement("div");
+        cells.style.width = `${gridSide / cols}px`;
+        cells.style.height = `${gridSide / rows}px`;
+        cells.classList.add("squares");
+        cells.textContent = '';
+        gridArea.appendChild(cells);
+        
+        cells.addEventListener("mouseenter", () => {
+            cells.style.backgroundColor = 'black';
+        })
     }
-    initBtn.removeEventListener("click", createSquares);
+    initBtn.removeEventListener("click", sixteenBySixteen);
+}
+
+
+//Creates Grid of custom size based on user input
+function customGrid(input) {
+    input = prompt('Enter a number between 1 and 100 for the number of pixels per side of the grid!');
+
+    //converts input type from string to number to be used later in comparisons
+    input = Number(input); 
+
+    //Squares the input number to make the correct amount of square divs for a grid
+    rows = input;
+    cols = input;
+
+    //Checks if user input is between 1 and 100
+    if (input >= 1 && input <= (100 * 100)){
+        let i = 255;
+        while (i >= 0) {
+            let removeSquares = document.getElementsByClassName("squares");
+            removeSquares[0].remove();
+            i--;
+        }
+        for (let num = 0; num < (rows * cols); num++) {
+            let cells = document.createElement("div");
+            cells.style.width = `${gridSide / cols}px`;
+            cells.style.height = `${gridSide / rows}px`;
+            cells.classList.add("squares");
+            cells.textContent = '';
+            gridArea.appendChild(cells);
+
+            cells.addEventListener("mouseenter", () => {
+                cells.style.backgroundColor = 'black';
+            });
+        }
+    }
+    else {
+        alert('Number outside of allowed range, please try again.');
+    }
+    inputBtn.removeEventListener("click", customGrid);
 }
 
 //Creates two circlular divs to represent the dials on an etch-a-sketch
@@ -33,8 +81,11 @@ function createDials() {
 
 
 let initBtn = document.getElementById("sixteen");
-initBtn.addEventListener("click", createSquares);
+initBtn.addEventListener("click", sixteenBySixteen);
 initBtn.addEventListener("click", createDials);
+
+let inputBtn = document.getElementById("gridSize");
+inputBtn.addEventListener("click", customGrid);
 
 
 
